a0a0d10a3ee70534c9a2371a497b8eda8e56f28e
DarlanJurak <darlan.jurak@gmail.com>
2019-07-24 20:00:44 -0300

    added workd frame to discover tf between map and odom

--- src/tf_broadcaster.cpp
+++ src/tf_broadcaster.cpp
@@ -16,8 +16,12 @@ int main(int argc, char** argv){
         ros::Time::now(),"base_link", "base_laser_link"));
     broadcaster.sendTransform(
       tf::StampedTransform(
-        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(11.5, 10, 0)),
-        ros::Time::now(),"map", "odom"));
+        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0, 0, 0)),
+        ros::Time::now(),"world", "map"));
+    broadcaster.sendTransform(
+      tf::StampedTransform(
+        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0, 0, 0)),
+        ros::Time::now(),"world", "odom"));
     r.sleep();
   }
 }
