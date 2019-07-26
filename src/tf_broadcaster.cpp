ba7453f13bcadadd591b214ece1c678b1f35850e
DarlanJurak <darlan.jurak@gmail.com>
2019-07-26 11:43:17 -0300

    removed world frame and corrected alignment between glocal and local costmaps

--- src/tf_broadcaster.cpp
+++ src/tf_broadcaster.cpp
@@ -14,14 +14,14 @@ int main(int argc, char** argv){
       tf::StampedTransform(
         tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.05, 0.0, 0.205)),
         ros::Time::now(),"base_link", "base_laser_link"));
+    // broadcaster.sendTransform(
+    //   tf::StampedTransform(
+    //     tf::Transform(tf::Quaternion(0, 0, 0.964, 0.267), tf::Vector3(0, 0, 0)),
+    //     ros::Time::now(),"world", "map"));
     broadcaster.sendTransform(
       tf::StampedTransform(
-        tf::Transform(tf::Quaternion(0, 0, 0.964, 0.267), tf::Vector3(0, 0, 0)),
-        ros::Time::now(),"world", "map"));
-    broadcaster.sendTransform(
-      tf::StampedTransform(
-        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0, 0, 0)),
-        ros::Time::now(),"world", "odom"));
+        tf::Transform(tf::Quaternion(0, 0.9635582, 0.2674988, 1), tf::Vector3(15.75, 14.3, 0)),
+        ros::Time::now(),"map", "odom"));
     r.sleep();
   }
 }
